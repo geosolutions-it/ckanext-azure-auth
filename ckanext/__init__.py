d001d33543a167005369bc42730f5ca44f6758c1
Serge Korotkin <korotkin.s@gmail.com>
2021-04-06 14:57:32 +0300

    Added init file
    Signed-off-by: Serge Korotkin <korotkin.s@gmail.com>

---
+++ ckanext/__init__.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+__version__ = '0.0.1'
+__description__ = 'CKAN Software'
+__long_description__ = \
+'''
+'''
+__license__ = 'AGPL'
+
+# The packaging system relies on this import, please do not remove it
+import sys; sys.path.insert(0, __path__[0])
+
+
+# this is a namespace package
+try:
+    import pkg_resources
+
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+
+    __path__ = pkgutil.extend_path(__path__, __name__)
